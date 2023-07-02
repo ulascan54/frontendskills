1eca4fed322b8a4adcf7bc732ac91212dc03d862
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-02 16:42:58 +0300

    the project finished

--- 10_dad_jokes/script.js
+++ 10_dad_jokes/script.js
@@ -2,5 +2,32 @@ const jokes=document.querySelector('.jokes')
 const jokeBtn=document.querySelector('.btn')
 
 generatenJoke()
+//using then
+// function generatenJoke(){
+//     const config={
+//         headers:{
+//             'Accept':'application/json'
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com/',config)
+//     .then((res)=>res.json())
+//     .then((data)=>{
+//         jokes.innerText=data.joke
+//     })
+// }
 
-function
+
+// using async await
+async function generatenJoke(){
+    const config={
+        headers:{
+            'Accept':'application/json'
+        }
+    }
+    const res = await fetch('https://icanhazdadjoke.com/',config)
+    const data = await res.json()
+    jokes.innerText=data.joke
+}
+
+
+jokeBtn.addEventListener('click',generatenJoke)
